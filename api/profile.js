1e1790da522b871962d3d242b2d433cd1efa496d
Ravinder Sirohi <Ravinder.Sirohi@hovergen.com>
2020-04-25 13:23:43 +0100

    jwt token endpoint added.

--- api/profile.js
+++ api/profile.js
@@ -6,13 +6,21 @@ routes.get('/', (req, resp, next) => {
     resp.send('GET - Profile!');
 });
 routes.post('/', (req, resp, next) => {
-    const { email } = req.body;
+    const { fullname, email } = req.body;
     console.log(req.body);
-    resp.send(`POST - Create ${email} profile!`);
+    resp.send(`POST - Create ${fullname} profile!`);
 });
 routes.post('/login', (req, resp, next) => {
     const { fullname, email } = req.body;
     console.log(req.body);
-    resp.send(`Loging - ${fullname}!`);
+    if (!fullname || !email) resp.status(401).send('Invalid user details!');
+    else {
+        jwt.sign({ fullname, email }, "aPikEy", (err, token) => {
+            if (err)
+                resp.status(400);
+            else
+                resp.status(200).json({ token });
+        })
+    }
 });
 module.exports = routes;
